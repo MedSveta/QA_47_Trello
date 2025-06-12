8d1ae58796bc035911da39cbeba37dfe359399d2
MedSveta <sveta1978medved@gmail.com>
2025-06-12 20:50:35 +0300

    LoginPage

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,4 +1,18 @@
 package tests;
 
-public class LoginTests {
+import dto.User;
+import manager.AppManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class LoginTests extends AppManager {
+    @Test
+    public void loginPositiveTest(){
+        User user = User.builder()
+                .email("sveta1978medved@gmail.com")
+                .password("Medqwerty12345!")
+                .build();
+        new HomePage(getDriver()).clickBtnLogin();
+
+    }
 }
