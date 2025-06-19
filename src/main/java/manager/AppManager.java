a50a136a9051be6686e61704b9520c4d4e62b92a
MedSveta <sveta1978medved@gmail.com>
2025-06-19 20:56:10 +0300

    Create Board New Board

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -2,10 +2,15 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.lang.reflect.Method;
+
 public class AppManager {
+    public Logger logger = LoggerFactory.getLogger(AppManager.class);
     private WebDriver driver;
 
     public WebDriver getDriver() {
@@ -13,14 +18,15 @@ public class AppManager {
     }
 
     @BeforeMethod
-    public void setup(){
+    public void setup(Method method){
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        logger.info("Start testing with method "+ method.getName());
     }
 
-
     @AfterMethod(enabled = false)
-    public void tearDown(){
+    public void tearDown(Method method){
+        logger.info("Stop testing "+ method.getName());
         if (driver != null)
             driver.quit();
     }
